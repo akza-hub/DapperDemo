00ba7dbb843e7046c8a8359ca9cd2f9d7452b1dd
Akza <akza.varghese@interfacesys.com>
2024-10-30 17:48:52 +0530

    Add project files.

---
+++ DapperDemo/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace DapperDemo.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+               public DbSet<Company> Companies { get; set; }
+         
+    }
+}
