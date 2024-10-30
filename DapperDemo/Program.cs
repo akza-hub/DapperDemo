168d15d099b24a1b2c360de1f2def8fd91776432
Akza <akza.varghese@interfacesys.com>
2024-10-30 20:15:27 +0530

    Added Dapper

--- DapperDemo/Program.cs
+++ DapperDemo/Program.cs
@@ -1,4 +1,5 @@
 using DapperDemo.Data;
+using DapperDemo.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+builder.Services.AddScoped<ICompanyRepository,CompanyRepository>();
 
 
 
---
+++ DapperDemo/Repository/CompanyRepositoryEF.cs
@@ -0,0 +1,49 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryEF : ICompanyRepository
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public CompanyRepositoryEF(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public Company Add(Company company)
+        {
+            _db.Add(company);
+            _db.SaveChanges();
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            return _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+        }
+
+        public List<Company> GetAll()
+        {
+            return _db.Companies.ToList();
+        }
+
+        public void Remove(int id)
+        {
+            Company company = _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+            _db.Companies.Remove(company);
+            _db.SaveChanges();
+            return;
+
+        }
+
+        public Company Update(Company company)
+        {
+            _db.Update(company);
+            _db.SaveChanges();
+            return company;
+        }
+    }
+}
